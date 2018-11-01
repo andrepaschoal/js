ada38bb44e285a735ba1b73f5d019ffed0992594
andrepaschoal <seuparada@gmail.com>
2018-11-01 23:15:59 +0000

    Update ca-angular.js

--- ca-angular.js
+++ ca-angular.js
@@ -2,6 +2,7 @@
 angular.module('CaApp', []).controller('CaController', function ($scope) {
     $scope.title = 'Aprovação de Campanha';
 
+    /*
     $scope.campaigns = [
         { id: 1, title: 'Julietto', partner: 'Julietto', approvedBy: null, approvalDate: null, sfId: null },
         { id: 2, title: 'Banco do Brasil', partner: 'Banco do Brasil', approvedBy: 'Josemando Sobral', approvalDate: '20/10/2018', sfId: null },
@@ -9,6 +10,12 @@ angular.module('CaApp', []).controller('CaController', function ($scope) {
         { id: 4, title: 'ALE', partner: 'ALE', approvedBy: null, approvalDate: null, sfId: null },
         { id: 5, title: 'BIBI', partner: 'BIBI', approvedBy: 'José Marconi', approvalDate: '12/09/2018', sfId: null },
     ];
+    */
+
+    fetch('https://raw.githubusercontent.com/andrepaschoal/js/master/campaigns.json').then(data => data.json()).then(data => {
+        $scope.campaigns = data;
+        $scope.$apply();
+    });
 
 });
 
