4a67abc64a56f3886597326dde3dca1e6ab5ac9b
andrepaschoal <seuparada@gmail.com>
2018-12-03 00:24:01 +0000

    Update campaign-creation-form.js

--- campaign-creation-form.js
+++ campaign-creation-form.js
@@ -1,5 +1,24 @@
-  console.log('opa');
+	$(document).ready(function () {
+		$('#CampTipoAcao').change(function (event) {
+			const val = $(this).val();
 
-$('#CampTipoAcao').change(function() {
-  console.log('TipoAcao');
-});
+			if (!val || (val && $('#preview-icon').hasClass('disabled'))) {
+				$('#preview-icon').toggleClass('enabled disabled');
+			}
+		});
+
+		$('#preview-icon-container').tooltip({ title: 'opa' });
+		$('#preview-icon').click(function () {
+			if ($(this).hasClass('disabled')) {
+				return;
+			}
+			console.log(1);
+
+			$('#img-preview img').attr('src', getImageSrc($('#CampTipoAcao').val()));
+			$('#img-preview').modal('show');
+		});
+	});
+
+	function getImageSrc(type) {
+		return $(`#${type}`).attr('src');
+	}
