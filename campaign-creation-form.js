3c4832c11cb63188176bf7fce6f711f653ff0d6f
andrepaschoal <seuparada@gmail.com>
2018-12-03 11:20:26 +0000

    Update campaign-creation-form.js

--- campaign-creation-form.js
+++ campaign-creation-form.js
@@ -1,20 +1,28 @@
-
+console.log('[cc] init');
+console.log('[cc] CampTipoAcao');
+console.log($('#CampTipoAcao'));
 $('#CampTipoAcao').change(function (event) {
-	const val = $(this).val();
+	console.log(`[cc][CampTipoAcao.change] handling event`);
 
+	const val = $(this).val();
+	console.log(`[cc][CampTipoAcao.change] val ${val}`);
 	if (!val || (val && $('#preview-icon').hasClass('disabled'))) {
+		console.log(`[cc][CampTipoAcao.change] toggling preview-icon class`);
 		$('#preview-icon').toggleClass('enabled disabled');
 	}
 });
 
 $('#preview-icon-container').tooltip({ title: 'opa' });
 $('#preview-icon').click(function () {
+	console.log(`[cc][PreviewIcon.click]`);
 	if ($(this).hasClass('disabled')) {
+		console.log(`[cc][PreviewIcon.click] disabled. returning`);
 		return;
 	}
-	console.log(1);
 
+	console.log(`[cc][PreviewIcon.click] getting image for ${$('#CampTipoAcao').val()}`);
 	$('#img-preview img').attr('src', getImageSrc($('#CampTipoAcao').val()));
+	console.log(`[cc][PreviewIcon.click] showing modal`);
 	$('#img-preview').modal('show');
 });
 
