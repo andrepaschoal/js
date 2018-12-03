2865291e454f1b5fe5c9e34ccde72ba0be5bbca6
andrepaschoal <seuparada@gmail.com>
2018-12-03 00:30:54 +0000

    Update campaign-creation-form.js

--- campaign-creation-form.js
+++ campaign-creation-form.js
@@ -1,24 +1,23 @@
-	$(document).ready(function () {
-		$('#CampTipoAcao').change(function (event) {
-			const val = $(this).val();
 
-			if (!val || (val && $('#preview-icon').hasClass('disabled'))) {
-				$('#preview-icon').toggleClass('enabled disabled');
-			}
-		});
+$('#CampTipoAcao').change(function (event) {
+	const val = $(this).val();
 
-		$('#preview-icon-container').tooltip({ title: 'opa' });
-		$('#preview-icon').click(function () {
-			if ($(this).hasClass('disabled')) {
-				return;
-			}
-			console.log(1);
-
-			$('#img-preview img').attr('src', getImageSrc($('#CampTipoAcao').val()));
-			$('#img-preview').modal('show');
-		});
-	});
-
-	function getImageSrc(type) {
-		return $(`#${type}`).attr('src');
+	if (!val || (val && $('#preview-icon').hasClass('disabled'))) {
+		$('#preview-icon').toggleClass('enabled disabled');
 	}
+});
+
+$('#preview-icon-container').tooltip({ title: 'opa' });
+$('#preview-icon').click(function () {
+	if ($(this).hasClass('disabled')) {
+		return;
+	}
+	console.log(1);
+
+	$('#img-preview img').attr('src', getImageSrc($('#CampTipoAcao').val()));
+	$('#img-preview').modal('show');
+});
+
+function getImageSrc(type) {
+	return $(`#${type}`).attr('src');
+}
